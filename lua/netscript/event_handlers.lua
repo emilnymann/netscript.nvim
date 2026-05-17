1398152c290077273975c3c52b9327bd69c390ca
Emil Nymann Sølyst <emilnymann96@gmail.com>
2026-05-17 15:09:07 +0200

    refactor(init): extract autocmd event handlers to module

---
+++ lua/netscript/event_handlers.lua
@@ -0,0 +1,36 @@
+local rpc = require("netscript.rpc")
+local ws = require("netscript.ws")
+
+local M = {}
+
+---@param ev vim.api.keyset.create_autocmd.callback_args
+function M.on_buf_write(ev)
+	if not ws._running then
+		return
+	end
+
+	local relative_dir = vim.fn.fnamemodify(ev.file, ":~:.:h")
+
+	local server = "home"
+	if relative_dir ~= "." then
+		server = relative_dir
+	end
+
+	local filename = vim.fs.basename(ev.file)
+	local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)
+	local contents = table.concat(lines, "\n")
+
+	rpc.push_file(filename, contents, server, function(err)
+		if err ~= nil then
+			vim.notify(
+				string.format("netscript: failed to push file <%s>\n%s", filename, err.message),
+				vim.log.levels.ERROR
+			)
+			return
+		end
+
+		vim.notify(string.format("netscript: pushed file <%s> to server <%s>", filename, server), vim.log.levels.DEBUG)
+	end)
+end
+
+return M
--- lua/netscript/init.lua
+++ lua/netscript/init.lua
@@ -1,6 +1,7 @@
 local ws = require("netscript.ws")
 local rpc = require("netscript.rpc")
 local cmd = require("netscript.cmd")
+local evh = require("netscript.event_handlers")
 
 local M = {}
 
@@ -56,37 +57,7 @@ function M.setup(opts)
 
 	vim.api.nvim_create_autocmd("BufWrite", {
 		group = group,
-		callback = function(ev)
-			if not ws._running then
-				return
-			end
-
-			local relative_dir = vim.fn.fnamemodify(ev.file, ":~:.:h")
-
-			local server = "home"
-			if relative_dir ~= "." then
-				server = relative_dir
-			end
-
-			local filename = vim.fs.basename(ev.file)
-			local lines = vim.api.nvim_buf_get_lines(ev.buf, 0, -1, false)
-			local contents = table.concat(lines, "\n")
-
-			rpc.push_file(filename, contents, server, function(err)
-				if err ~= nil then
-					vim.notify(
-						string.format("netscript: failed to push file <%s>\n%s", filename, err.message),
-						vim.log.levels.ERROR
-					)
-					return
-				end
-
-				vim.notify(
-					string.format("netscript: pushed file <%s> to server <%s>", filename, server),
-					vim.log.levels.DEBUG
-				)
-			end)
-		end,
+		callback = evh.on_buf_write,
 	})
 
 	cmd.setup()
